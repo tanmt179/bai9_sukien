c4c1e7b60fe13029abbfab6d7bb9b988a71816da
tanmt179 <106754526+tanmt179@users.noreply.github.com>
2022-06-12 10:14:18 +0700

    Add files via upload

---
+++ if1.js
@@ -0,0 +1,54 @@
+//let a = prompt("nhập giá trị a");
+//let b = prompt("Nhập giá trị b");
+//let c = a%b;
+//if (c==0){
+//   document.write('a chia hết cho b')
+//}else {
+//   document.write('a không chia hết cho b')
+//}
+//.................................................
+//let old = prompt("nhập số tuổi");
+//if (old<15){
+//    document.write('Không đủ điều kiện học lớp 10')
+//}
+//......................................................
+//let a=prompt('Nhập một số bất kì');
+//if (a<0){
+//   document.write('số nhỏ hơn 0')
+//}else {
+//   document.write('so lon hon 0')
+//}
+//........................................................
+//let a=prompt('nhap so thu 1');
+//let b=prompt('nhap so thu 2');
+//let c=prompt('nhap so thu 3');
+//if/a>b && a>c){
+//    max=a;
+//}else if(b>c&&b>a){
+//   max=b;
+//}else {
+//    max=c;
+//}document.getElementById('in').innerHTML='so lon nhat la: '+max;
+//...................................................................
+//let a=prompt('nhap diem kiem tra');
+//let b=prompt('nhap diem thi giua ky');
+//let c=prompt('nhap diem kiem tra');
+//if (a>=9&&b>=9&&c>=9){
+//   max='gioi';
+//}else if(a>=7&&b>=7&&c>=7){
+//   max='kha';
+//}else{
+//   max='trung binh'
+//}document.getElementById('in').innerHTML='hoc luc:  '+max;
+//.......................................................
+//let a=prompt('nhap doanh so ban hang');
+//if(a<5000000){
+ //   money=a*3/100;
+//}else if(a<7500000){
+ //   money=a*5/100;
+//}else if(a<10000000){
+//    money=a*7/100;
+//}else {money=a*10/100}
+//document.getElementById('in').innerHTML='hoa hong nhan duoc: '+money;
+//............................................
+
